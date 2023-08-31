171c81f52f0854011583c31286c2b804d34dcb52
ArinSwagg <rakuzanoat@gmail.com>
2023-08-31 20:12:04 +0700

    re-link

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,12 +6,12 @@ const Header = (props)=>{
             <span className="logo"><Link to="/">TAXI UBR Thailand</Link></span>
             <nav>
                 <ul className="nav_link">
-                    <li><Link to="tour">Tourist Attraction</Link></li>
-                    <li><Link to="reviews">Reviews</Link></li>
-                    <li><Link to="about">About Us</Link></li>
+                    <li><Link to="/tour">Tourist Attraction</Link></li>
+                    <li><Link to="/reviews">Reviews</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
                 </ul>
             </nav>
-            <a className="cta" href="about"><button>Contact Us</button></a>
+            <a className="cta" href="/about"><button>Contact Us</button></a>
         </header>
     )
 }
