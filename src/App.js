440ec4fc61a2bc457bb67c6e32ecf4be3cc0d5b4
kiok127523 <kiok127523@gmail.com>
2023-09-08 16:41:07 +0700

    takayai naja
    test

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import Content from './component/Content';
 import Destination from './component/Destination';
 import Map from './component/Map';
 import Vector from './component/Vector';
+import Travel from './component/Travel';
+import Nav from './component/Nav';
 
 function App() {
   return (
-    <div className='main'>
-      <Vector/>
-      <Header/>
-      <div className='content'>
-        <Hero/>
-        <Destination/>
-        <Content/>
+    <>
+      {/* <div className='main'>
+        <Vector />
+        <Header />
+        <div className='content'>
+          <Hero />
+          <Destination />
+          <Content />
+        </div>
+        <Map/>
+      </div> */}
+
+      {/* ด้านล่างของกู */}
+      
+      <div className='main'>
+        <Nav />
+        <Travel />
       </div>
-      {/* <Map/> */}
-    </div>
+    </>
   );
 }
 
