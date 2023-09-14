079510b336b2359853845d0a874b93070618b821
ArinSwagg <rakuzanoat@gmail.com>
2023-09-14 18:58:52 +0700

    fix margin

--- src/component/hero/DestinationGallery.js
+++ src/component/hero/DestinationGallery.js
@@ -1,45 +1,28 @@
-import React, { useState } from 'react';
-import ImageModal from "./ImageModal";
+import { Link } from "react-router-dom";
 export default ()=>{
-    const [modalShow, setModalShow] = useState(false);
-    const [selectedImage, setSelectedImage] = useState('');
-
-    const openNewTab = (url) => {
-        window.location.href = url;
-        // setSelectedImage(imageUrl);
-        // setModalShow(true);
-    };
-
-    const closeModal = () => {
-        setSelectedImage('');
-        setModalShow(false);
-    };
-
     return(
     <>
         <div className="car-gallery">
             <div class="car-gallery-row">
                 <div className="car-gallery-item">
-                    {/* <span class="badge rounded-pill bg-primary">Primary</span> */}
                     <h2 className='destination-desc'>สระมรกต</h2>
-                    <img src={require('../img/Blue_Lagoon1.jpg')} onClick={() => openNewTab("/bluelagoon")}/>
+                    <Link className="clearBtn" to={"/bluelagoon"}><img src={require('../img/Blue_Lagoon1.jpg')} /></Link>
                 </div>
                 <div className="car-gallery-item">
                     <h2 className='destination-desc'>เขาค้อ</h2>
-                    <img src={require('../img/Khao_Kho1.jpg')} onClick={() => openNewTab("/phamanfun")}/>
+                    <Link className="clearBtn" to={"/khaokho"}><img src={require('../img/Khao_Kho1.jpg')} /></Link>
                 </div>
             </div>
             <div class="car-gallery-row">
                 <div className="car-gallery-item">
                     <h2 className='destination-desc'>ช้างอารีนา</h2>
-                    <img src={require('../img/Chang_Arena_Buriram1.jpg')} onClick={() => openNewTab("/changarena")}/>
+                    <Link className="clearBtn" to={"/changarena"}><img src={require('../img/Chang_Arena_Buriram1.jpg')} /></Link>
                 </div>
                 <div className="car-gallery-item">
                     <h2 className='destination-desc'>ผาม่านฝัน</h2>
-                    <img src={require('../img/Phamanfun2.jpg')} onClick={() => openNewTab("/khaokho")}/>
+                    <Link className="clearBtn" to={"/phamanfun"}><img src={require('../img/Phamanfun2.jpg')} /></Link>
                 </div>
             </div>
-            <ImageModal show={modalShow} onHide={closeModal} imageUrl={selectedImage} />
         </div>
     </>
     )
